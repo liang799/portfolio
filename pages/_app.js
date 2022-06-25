792b53233d1b261cc8797e8aaf0adf422357b95a
liang799 <71926348+liang799@users.noreply.github.com>
2022-06-26 02:49:39 +0800

    Overidden global App component

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default App;
