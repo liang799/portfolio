c0748a507589f088afa84e1dfb19b660151284fb
liang799 <71926348+liang799@users.noreply.github.com>
2022-06-26 20:50:30 +0800

    working on hero section

---
+++ components/hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { 
+  Container,
+  Text,
+} from "@chakra-ui/react";
+import Image from "next/image";
+
+export default function HeroSection() {
+  return (
+    <Container maxWidth="container.xl">
+      <Text>laskdjflkasdjflkasjdf</Text>
+    </Container>
+  );
+}
