72e96ed20d474ef1e24d8b5221c0f0304f4871ac
liang799 <71926348+liang799@users.noreply.github.com>
2022-06-28 02:11:35 +0800

    Edited fonts

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,14 @@ import theme from "../styles/theme";
 const App = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
+      <Global
+        styles={css`
+          @font-face {
+            font-family: "Rage";
+            src: url("/fonts/RageItalic.woff") format("woff");
+          }
+        `}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   );
