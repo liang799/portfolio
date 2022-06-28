01292de89e0f125c32f71b2cd635e9dba12b1a67
liang799 <71926348+liang799@users.noreply.github.com>
2022-06-28 11:18:46 +0800

    Portfolio section is not opitmised for mobile

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -5,6 +5,7 @@ import {
   Container,
   Image,
   SimpleGrid,
+  Link,
   Box,
   VStack,
   Flex,
@@ -18,7 +19,7 @@ export default function Hero() {
       <Container maxWidth="container.xl" py={4} px={8}>
         <Heading
           pt={10}
-          pb={5}
+          pb={8}
           m="1px"
           _before={{
             content: '""',
@@ -32,20 +33,27 @@ export default function Hero() {
         >
           Portfolio
         </Heading>
-        <SimpleGrid columns={{ sm: 2, md: 3 }} spacingX="40px" spacingY="40px">
-          {PROJECTS.map(({ src, alt }) => (
-            <Image
-              key={src}
-              src={src}
-              alt={alt}
-              fallback={<Skeleton />}
-              width="400px"
-              height="400px"
-              rounded="xl"
-              objectFit="cover"
-              opacity={0.5}
-              _hover={{ opacity: 1 }}
-            />
+        <SimpleGrid
+          columns={{ sm: 2, md: 3 }}
+          spacingX="40px"
+          spacingY="40px"
+          pb={10}
+        >
+          {PROJECTS.map(({ link, src, alt }) => (
+            <Link href={link} isExternal>
+              <Image
+                key={src}
+                src={src}
+                alt={alt}
+                fallback={<Skeleton />}
+                width="400px"
+                height="400px"
+                rounded="xl"
+                objectFit="cover"
+                opacity={0.5}
+                _hover={{ opacity: 1 }}
+              />
+            </Link>
           ))}
         </SimpleGrid>
       </Container>
@@ -55,26 +63,32 @@ export default function Hero() {
 
 const PROJECTS = [
   {
+    link: "https://github.com/liang799/Vigour",
     src: "/images/vigour.png",
     alt: "Vigour Project showcase",
   },
   {
+    link: "https://www.bellcurvehero.com/copyright",
     src: "/images/bellcurvehero.png",
     alt: "BellCurveHero Internship showcase",
   },
   {
+    link: "https://github.com/liang799/CSAD-Project",
     src: "/images/tree.png",
     alt: "CSAD Project showcase",
   },
   {
+    link: "https://xd.adobe.com/view/0dccdb83-fb9c-49de-bc8d-34652a9a20e9-589f/",
     src: "/images/poketeams.png",
     alt: "Hobby Project showcase",
   },
   {
+    link: "https://github.com/liang799/mental-health-sol",
     src: "/images/bot.png",
     alt: "Discord bot showcase",
   },
   {
+    link: "https://github.com/liang799/portfolio",
     src: "/images/website.png",
     alt: "This website",
   },
