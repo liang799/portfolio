5766de0b9b3783bd68cc2c8a1854af0e5db5ff98
liang799 <71926348+liang799@users.noreply.github.com>
2022-07-05 07:01:17 +0800

    Working on integrating netlify form into application

--- components/Form.js
+++ components/Form.js
@@ -8,27 +8,49 @@ import {
   Stack,
   Button,
 } from "@chakra-ui/react";
+import { useForm, ValidationError } from "@formspree/react";
 
-export default function Form() {
+export default function ContactForm() {
+  const [state, handleSubmit] = useForm("YOUR_FORM_ID");
+
+  if (state.succeeded) {
+    return <Text color="white">Thanks for your submission!</Text>;
+  }
+
   return (
     <Stack>
-      <Stack spacing={4}>
-        <FormControl color="white" id="name">
-          <FormLabel> Name</FormLabel>
-          <Input bg={"black.form"} border={0} type="" />
-        </FormControl>
-        <FormControl color="white" id="email">
-          <FormLabel>Email</FormLabel>
-          <Input bg={"black.form"} border={0} type="email" />
-        </FormControl>
-        <FormControl color="white" id="msg">
-          <FormLabel>Message</FormLabel>
-          <Textarea bg={"black.form"} border={0} h={40} type="message" />
-        </FormControl>
-        <Stack pt={2} pb={10}>
-          <Button colorScheme="primary">Send Message</Button>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={4}>
+          <FormControl color="white" id="name" name="name">
+            <FormLabel> Name</FormLabel>
+            <Input bg={"black.form"} border={0} />
+          </FormControl>
+          <ValidationError prefix="Name" field="name" errors={state.errors} />
+
+          <FormControl color="white" id="email" name="email">
+            <FormLabel>Email</FormLabel>
+            <Input bg={"black.form"} border={0} type="email" />
+          </FormControl>
+          <ValidationError prefix="Email" field="email" errors={state.errors} />
+
+          <FormControl color="white" id="message" name="message">
+            <FormLabel>Message</FormLabel>
+            <Textarea bg={"black.form"} border={0} h={40} type="message" />
+          </FormControl>
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
+          />
+
+          <Stack pt={2} pb={10}>
+            <Button colorScheme="primary" type="submit" disabled={state.submitting}>
+              Send Message
+            </Button>
+          </Stack>
+          <ValidationError errors={state.errors} />
         </Stack>
-      </Stack>
+      </form>
     </Stack>
   );
 }
