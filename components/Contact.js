01c24b50ddf0f8609995f71e370abbb9c86ac86c
liang799 <71926348+liang799@users.noreply.github.com>
2022-07-05 05:33:57 +0800

    Completed UI for form section

--- components/Contact.js
+++ components/Contact.js
@@ -1,4 +1,7 @@
 import {
+  HStack,
+  Link,
+  SimpleGrid,
   Box,
   Flex,
   Container,
@@ -11,7 +14,7 @@ import Form from "./Form";
 
 export default function Contact() {
   return (
-    <Flex maxWidth="full" bg="black.main" p={0}>
+    <Flex maxWidth="full" bg="black.main" p={0} pb={10}>
       <Container
         flex={1}
         maxWidth="container.xl"
@@ -41,17 +44,40 @@ export default function Contact() {
           <Form />
         </Stack>
       </Container>
-      <Flex bg="black.alt" flex={1.2} justify={"center"}>
-        <Stack pt={40} pl={{base: 10, lg:20}} pr={{base:10, lg:20}}>
-          <Text color="white">
-            Like what you see? Have any queries? Feel free to contact me!
-            I am usually active on email and LinkedIn.
+      <Flex bg="black.alt" flex={1.2} align="center" justify={"center"}>
+        <Stack pl={{ base: 10, lg: 20 }} pr={{ base: 10, lg: 20 }}>
+          <Text color="white" pb={5}>
+            Like what you see? Have any queries? Feel free to contact me! I am
+            usually active on email and LinkedIn.
           </Text>
-          <Box color="white" className="fas fa-envelope" />
-          <Box color="white" className="fab fa-linkedin-in" />
-          <Box color="white" className="fab fa-github" />
+          {OTHER.map(({ index, icon, href, text }) => (
+            <HStack index={index} spacing={5}>
+              <Box color="white" className={icon} />
+              <Link color="white" href={href} isExternal>
+                {text}
+              </Link>
+            </HStack>
+          ))}
         </Stack>
       </Flex>
     </Flex>
   );
 }
+
+const OTHER = [
+  {
+    icon: "fas fa-envelope",
+    href: "mailto:hello@tianpok.com",
+    text: "hello@tianpok.com",
+  },
+  {
+    icon: "fab fa-linkedin-in",
+    href: "https://www.linkedin.com/in/tianpok",
+    text: "linkedin.com/in/tianpok",
+  },
+  {
+    icon: "fab fa-github",
+    href: "https://github.com/liang799",
+    text: "github.com/liang799",
+  },
+];
